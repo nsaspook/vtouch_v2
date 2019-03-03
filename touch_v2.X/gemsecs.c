804a8591808a42fdf42dc03fc53b6227401a8dc5
nsaspook <nsaspook@nsaspook.com>
2019-03-02 19:57:46 -0800

    update

--- touch_v2.X/gemsecs.c
+++ touch_v2.X/gemsecs.c
@@ -1,1 +1,31 @@
+/*
+ Tcl code for secs  secs-0.2.0.tm
+ */
 
+
+/*
+ checksum generation
+ 
+ ::set sum 0
+                                binary scan $msg1 c* x
+                                ::foreach e $x {
+                                        incr sum [expr {$e&255}]
+                                }
+ */
+
+/*
+ * checksum checking
+ * load sum with received checksum value and subtract to zero
+ 
+ 
+ binary scan [string range $data $n-2 $n-1] S sum
+                                binary scan [string range $data 1 10] c* x
+                                ::foreach e $x {
+                                        incr sum [expr {-($e&255)}]
+                                }
+                                ::set result [string range $data 11 $n-3]
+                                binary scan $result c* x
+                                ::foreach e $x {
+                                        incr sum [expr {-($e&255)}]
+                                }
+ */
