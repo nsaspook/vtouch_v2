ee1cb045d49bcf88a0ab0c32fc71747c493cee06
nsaspook <nsaspook@nsaspook.com>
2019-09-05 16:34:07 -0700

    up

--- touch_board/elo_touch.X/build/default/production/d232.i
+++ touch_board/elo_touch.X/build/default/production/d232.i
@@ -27077,7 +27077,7 @@ _Bool Digital232_RW(void)
 
  StartTimer(TMR_SPS, 10 + IO.speed + IO.slower);
 
- ADCC_StartConversion(channel_FVR_Buffer2);
+ ADCC_StartConversion(channel_ANA0);
  while (!ADCC_IsConversionDone());
  IO.button_value = ADCC_GetConversionResult();
 
--- touch_board/elo_touch.X/d232.c
+++ touch_board/elo_touch.X/d232.c
@@ -45,7 +45,7 @@ bool Digital232_RW(void)
 
 	StartTimer(TMR_SPS, 10 + IO.speed + IO.slower); // samples per second timer 10ms spacing at least
 
-	ADCC_StartConversion(channel_FVR_Buffer2);
+	ADCC_StartConversion(channel_ANA0);
 	while (!ADCC_IsConversionDone());
 	IO.button_value = ADCC_GetConversionResult();
 
