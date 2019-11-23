c464d52e3cffd0c9032ecd200603727f391d0d7d
nsaspook <nsaspook@nsaspook.com>
2019-11-23 14:06:46 -0800

    up

---
+++ touch_v2.X/dio.c
@@ -0,0 +1,125 @@
+#include "dio.h"
+#include "mcc_generated_files/ext_int.h"
+
+typedef struct S_data { // switch control data structure 
+	volatile rbutton_type button[MAX_BUTTONS];
+	volatile uint8_t sw_bitmap;
+} S_data;
+
+static S_data S;
+extern V_data V;
+
+static void switch_handler(void);
+
+/*
+ * return the current actual state of x switch
+ * the actual current state of a switch is ONLY changed in the interrupt handler
+ */
+SW_STATES get_switch(uint8_t i)
+{
+	if (i >= NUM_SWITCHES)
+		return SW_OFF;
+
+	return S.button[i].sw;
+}
+
+/*
+ * return the x switch structure
+ * mainly for testing
+ */
+rbutton_type get_switch_data(uint8_t i)
+{
+	if (i >= NUM_SWITCHES)
+		return S.button[SNULL];
+
+	return S.button[i];
+}
+
+/*
+ * check the switch pressed bitmap for button presses, the actual current switch state might differ
+ */
+uint8_t check_switches(void)
+{
+	return S.sw_bitmap;
+}
+
+/*
+ * clear X switch pressed and time pressed data
+ */
+void clear_switch(uint8_t i)
+{
+	if (i >= NUM_SWITCHES)
+		return;
+
+	S.button[i].count = 0;
+	S.sw_bitmap &= ~(1 << i); //clear switch pressed bit
+}
+
+void switch_handler(void)
+{
+	uint8_t i = 0, sw_value;
+
+	/*
+	 * enable the outputs for reading and reset MAX Change-of-State pin
+	 */
+	MAX_EN_SetLow();
+
+#ifdef DEBUG_SWH1
+	DEBUG1_SetHigh();
+#endif
+
+	// Nop for MAX chip output Propagation Delay after enable
+	Nop();
+	Nop();
+	Nop();
+	// start reading the various pic port input bits after the max chip is ready
+	do {
+		switch (i) {
+		case SENTER:
+			sw_value = ENTER_B_GetValue();
+			break;
+		case SSELECT:
+			sw_value = SELECT_B_GetValue();
+			break;
+		case S1:
+			sw_value = IO_RF1_GetValue();
+			break;
+		case S0:
+			sw_value = IO_RF0_GetValue();
+			break;
+		default:
+			sw_value = 1;
+			break;
+		}
+
+		// update actual current button state
+		if (sw_value) {
+			if (S.button[i].sw == SW_ON) {
+				S.button[i].sw = SW_OFF;
+			}
+		} else {
+			if (S.button[i].sw == SW_OFF) {
+				S.button[i].sw = SW_ON;
+				S.button[i].count = V.timerint_count; // so we can check button SW_ON duration
+				S.sw_bitmap |= 1 << i; // set switch pressed bit
+			}
+		}
+	} while (++i < NUM_SWITCHES);
+
+#ifdef DEBUG_SWH1
+	DEBUG1_SetLow();
+#endif
+#ifdef DEBUG_SWH2
+	DEBUG2_Toggle();
+#endif
+
+	MAX_EN_SetHigh(); // disable MAX output pins
+}
+
+void start_switch_handler(void)
+{
+	EXT_INT1_InterruptDisable();
+	INT1_SetInterruptHandler(switch_handler);
+	S.button[SNULL].sw = SW_INVALID; // set a error condition for invalid button number
+	EXT_INT1_InterruptEnable();
+}
---
+++ touch_v2.X/dio.h
@@ -0,0 +1,52 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef DIO_H
+#define	DIO_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include "vconfig.h"
+#include "tests.h"
+
+#define NUM_SWITCHES		4
+
+typedef struct rbutton_type {
+	SW_STATES sw;
+	uint32_t count;
+} rbutton_type;
+
+void start_switch_handler(void);
+SW_STATES get_switch(uint8_t);
+rbutton_type get_switch_data(uint8_t);
+uint8_t check_switches(void);
+void clear_switch(uint8_t);
+
+#endif	/* DIO_H */
+
