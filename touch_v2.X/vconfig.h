e6c2bdf66c8a264030ac27040b937331e6da7b72
nsaspook <nsaspook@nsaspook.com>
2020-01-24 08:15:18 -0800

    up

--- touch_v2.X/vconfig.h
+++ touch_v2.X/vconfig.h
@@ -20,7 +20,7 @@ extern "C" {
 #include "mcc_generated_files/pin_manager.h"
 #include "ringbufs.h"
 
-#define VER	"1.14"
+#define VER	"1.15"
 	/*
 	 * 0.1 MBMC new version for one 24vdc battery string for the 57K42
 	 * 0.2 start to configure the hardware for mbmc duty
