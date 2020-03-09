21d957b24ee39216b683e68fd885a17b19975637
root <bcddmp>
2020-03-09 15:00:37 -0700

    up

---
+++ q43_board/q43_baseline.X/mcc_generated_files/pin_manager.h
@@ -0,0 +1,137 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.79.0
+        Device            :  PIC18F47Q43
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 2.10 and above
+        MPLAB 	          :  MPLAB X 5.30	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set LED2 aliases
+#define LED2_TRIS                 TRISDbits.TRISD1
+#define LED2_LAT                  LATDbits.LATD1
+#define LED2_PORT                 PORTDbits.RD1
+#define LED2_WPU                  WPUDbits.WPUD1
+#define LED2_OD                   ODCONDbits.ODCD1
+#define LED2_ANS                  ANSELDbits.ANSELD1
+#define LED2_SetHigh()            do { LATDbits.LATD1 = 1; } while(0)
+#define LED2_SetLow()             do { LATDbits.LATD1 = 0; } while(0)
+#define LED2_Toggle()             do { LATDbits.LATD1 = ~LATDbits.LATD1; } while(0)
+#define LED2_GetValue()           PORTDbits.RD1
+#define LED2_SetDigitalInput()    do { TRISDbits.TRISD1 = 1; } while(0)
+#define LED2_SetDigitalOutput()   do { TRISDbits.TRISD1 = 0; } while(0)
+#define LED2_SetPullup()          do { WPUDbits.WPUD1 = 1; } while(0)
+#define LED2_ResetPullup()        do { WPUDbits.WPUD1 = 0; } while(0)
+#define LED2_SetPushPull()        do { ODCONDbits.ODCD1 = 0; } while(0)
+#define LED2_SetOpenDrain()       do { ODCONDbits.ODCD1 = 1; } while(0)
+#define LED2_SetAnalogMode()      do { ANSELDbits.ANSELD1 = 1; } while(0)
+#define LED2_SetDigitalMode()     do { ANSELDbits.ANSELD1 = 0; } while(0)
+
+// get/set BLED aliases
+#define BLED_TRIS                 TRISEbits.TRISE0
+#define BLED_LAT                  LATEbits.LATE0
+#define BLED_PORT                 PORTEbits.RE0
+#define BLED_WPU                  WPUEbits.WPUE0
+#define BLED_OD                   ODCONEbits.ODCE0
+#define BLED_ANS                  ANSELEbits.ANSELE0
+#define BLED_SetHigh()            do { LATEbits.LATE0 = 1; } while(0)
+#define BLED_SetLow()             do { LATEbits.LATE0 = 0; } while(0)
+#define BLED_Toggle()             do { LATEbits.LATE0 = ~LATEbits.LATE0; } while(0)
+#define BLED_GetValue()           PORTEbits.RE0
+#define BLED_SetDigitalInput()    do { TRISEbits.TRISE0 = 1; } while(0)
+#define BLED_SetDigitalOutput()   do { TRISEbits.TRISE0 = 0; } while(0)
+#define BLED_SetPullup()          do { WPUEbits.WPUE0 = 1; } while(0)
+#define BLED_ResetPullup()        do { WPUEbits.WPUE0 = 0; } while(0)
+#define BLED_SetPushPull()        do { ODCONEbits.ODCE0 = 0; } while(0)
+#define BLED_SetOpenDrain()       do { ODCONEbits.ODCE0 = 1; } while(0)
+#define BLED_SetAnalogMode()      do { ANSELEbits.ANSELE0 = 1; } while(0)
+#define BLED_SetDigitalMode()     do { ANSELEbits.ANSELE0 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
